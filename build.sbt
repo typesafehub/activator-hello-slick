4f229f4399d66f83b60923dfdcc4c1a30a7b7ff6
James Ward <james@jamesward.org>
2013-12-04 11:58:43 -0700

    added tests

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.10.2"
 libraryDependencies ++= List(
   "com.typesafe.slick" %% "slick" % "2.0.0-M3",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
-  "com.h2database" % "h2" % "1.3.170"
+  "com.h2database" % "h2" % "1.3.170",
+  "org.scalatest" %% "scalatest" % "2.0"
 )
---
+++ src/main/scala/Tables.scala
@@ -0,0 +1,25 @@
+import scala.slick.driver.H2Driver.simple._
+
+// A Suppliers table with 6 columns: id, name, street, city, state, zip
+class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
+  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+  def name = column[String]("SUP_NAME")
+  def street = column[String]("STREET")
+  def city = column[String]("CITY")
+  def state = column[String]("STATE")
+  def zip = column[String]("ZIP")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (id, name, street, city, state, zip)
+}
+
+// A Coffees table with 5 columns: name, supplier id, price, sales, total
+class Coffees(tag: Tag) extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
+  def name = column[String]("COF_NAME", O.PrimaryKey)
+  def supID = column[Int]("SUP_ID")
+  def price = column[Double]("PRICE")
+  def sales = column[Int]("SALES")
+  def total = column[Int]("TOTAL")
+  def * = (name, supID, price, sales, total)
+  // A reified foreign key relation that can be navigated to create a join
+  def supplier = foreignKey("SUP_FK", supID, TableQuery[Suppliers])(_.id)
+}
