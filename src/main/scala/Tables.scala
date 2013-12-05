e10bf1916700fe3c51c312ba080d52382ba94b0b
James Ward <james@jamesward.org>
2013-12-05 08:30:49 -0700

    reworked with more examples

--- src/main/scala/Tables.scala
+++ src/main/scala/Tables.scala
@@ -1,25 +1,30 @@
 import scala.slick.driver.H2Driver.simple._
+import scala.slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 // A Suppliers table with 6 columns: id, name, street, city, state, zip
 class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-  def name = column[String]("SUP_NAME")
-  def street = column[String]("STREET")
-  def city = column[String]("CITY")
-  def state = column[String]("STATE")
-  def zip = column[String]("ZIP")
+  def id: Column[Int] = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+  def name: Column[String] = column[String]("SUP_NAME")
+  def street: Column[String] = column[String]("STREET")
+  def city: Column[String] = column[String]("CITY")
+  def state: Column[String] = column[String]("STATE")
+  def zip: Column[String] = column[String]("ZIP")
+  
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (id, name, street, city, state, zip)
+  def `*`: ProvenShape[(Int, String, String, String, String, String)] = (id, name, street, city, state, zip)
 }
 
 // A Coffees table with 5 columns: name, supplier id, price, sales, total
 class Coffees(tag: Tag) extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
-  def name = column[String]("COF_NAME", O.PrimaryKey)
-  def supID = column[Int]("SUP_ID")
-  def price = column[Double]("PRICE")
-  def sales = column[Int]("SALES")
-  def total = column[Int]("TOTAL")
-  def * = (name, supID, price, sales, total)
+  def name: Column[String] = column[String]("COF_NAME", O.PrimaryKey)
+  def supID: Column[Int] = column[Int]("SUP_ID")
+  def price: Column[Double] = column[Double]("PRICE")
+  def sales: Column[Int] = column[Int]("SALES")
+  def total: Column[Int] = column[Int]("TOTAL")
+  
+  def `*`: ProvenShape[(String, Int, Double, Int, Int)] = (name, supID, price, sales, total)
+  
   // A reified foreign key relation that can be navigated to create a join
-  def supplier = foreignKey("SUP_FK", supID, TableQuery[Suppliers])(_.id)
+  def supplier: ForeignKeyQuery[Suppliers, (Int, String, String, String, String, String)] = 
+    foreignKey("SUP_FK", supID, TableQuery[Suppliers])(_.id)
 }
--- src/test/scala/TablesSuite.scala
+++ src/test/scala/TablesSuite.scala
@@ -15,7 +15,7 @@ class TablesSuite extends FunSuite with BeforeAndAfter {
   def insertSupplier(): Int = suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199")
   
   before {
-    session = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") createSession()
+    session = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver").createSession()
   }
   
   test("Creating the Schema works") {
